537b3c2c958d57f8154a91835e2063976d613130
Dallin Snell <scarhead7@users.noreply.github.com>
2025-11-22 15:41:00 -0700

    Project Update
    - updated makefile order
    - added fizzbuzz logic in main file (fizzbuzz.cpp)
    - may include residual executable file

--- C++/FizzBuzz/fizzbuzz.cpp
+++ C++/FizzBuzz/fizzbuzz.cpp
@@ -1,11 +1,20 @@
 /* CREATED BY: 1E1DB14C
-** CREATED 221314 NOV 2025
+** CREATED 221314 NOV 2025 UPDATED 22 NOV 2025
 ** FizzBuzz
 */
 
 
 #include <iostream>
 
-int main() {
+using std::cout;
+using std::endl;
+
+int main(void) {
+	/* Start at 1 and print numbers up to and including 20.
+	*/
+	for(int lcv = 1; lcv <= 20; lcv++) {
+		cout << lcv << endl;
+	} // end of for loop
+
 	return 0;
-}//end of main()
+} // end of main()
