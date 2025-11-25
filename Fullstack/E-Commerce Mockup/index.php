76496b8cca46b39153fcfc2dfb5c7a1154da1e90
Dallin Snell <scarhead7@users.noreply.github.com>
2025-11-25 13:25:02 -0700

    Updated Project
    - created index.php
    - deleted placeholder

---
+++ Fullstack/E-Commerce Mockup/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 1E1DB14C
+** CREATED 251302 NOV 2025 UPDATED 25 NOV 2025
+** 
+*/
+
+/*******TODO*****
+- 
+****************/
+
+session_start();
+
+
+?>
+
+<html>
+<p>Header</p>
+</html>
