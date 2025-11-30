84d321f84b66717dffc2125e961692d84d91c69e
Dallin Snell <scarhead7@users.noreply.github.com>
2025-11-30 14:55:06 -0700

    User Portal update
    - created personal.php
      - updates user personal information
    - updated style classes on user portal page
    - update pages links on user portal page

---
+++ Fullstack/E-Commerce Mockup/view/personal.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 1E1DB14C
+** CREATED 301443 NOV 2025 UPDATED 30 NOV 2025
+** Fullstack project: user account personal information page.
+*/
+
+/*******TODO*****
+ * - 
+****************/
+
+$title = "Personal Info";
+
+include $_SERVER['DOCUMENT_ROOT'] . '/Store/elements/header.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/Store/elements/nav.php';
+?>
+
+
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/Store/elements/footer.php';
+?>
--- Fullstack/E-Commerce Mockup/view/portal.php
+++ Fullstack/E-Commerce Mockup/view/portal.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 1E1DB14C
-** CREATED 261327 NOV 2025 UPDATED 29 NOV 2025
+** CREATED 261327 NOV 2025 UPDATED 30 NOV 2025
 ** Fullstack project: user account portal page.
 */
 
@@ -17,8 +17,8 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Store/elements/nav.php';
 
 <div class="account-links-container"><p>Orders</p></div>
 <div class="account-links-container"><p class="account-home-links">Delivery Addresses</p></div>
-<p class="account-home-links">Personal Information</p>
-<p class=""></p>
+<div class="account-links-container"><a class="account-home-links" href="/Store/view/personal.php">Personal Information</a></div>
+<div class="account-links-container"><p class="account-home-links">Login Information</p></div>
 
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/Store/elements/footer.php';
