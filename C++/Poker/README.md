305edbfa2dc63a500c784a688213c7a2bbcb9590
Dallin Snell <scarhead7@users.noreply.github.com>
2025-12-03 09:10:51 -0700

    Created Base Files
    - created makefile
    - created poker base file (poker.cpp)
    - created README

---
+++ C++/Poker/README.md
@@ -0,0 +1,9 @@
+# CLI-Poker
+Allows for a simple poker game to be played from a CLI.
+
+## TODOS:
+- [ ] create card class
+- [ ] create hand
+- [ ] create card/hand recognition logic
+<!-- - [ ] 
+-->
---
+++ C++/Poker/poker.cpp
@@ -0,0 +1,16 @@
+/* CREATED BY: 1E1DB14C
+** CREATED 030905 DEC 2025 UPDATED 03 DEC 2025
+** CLI Poker
+*/
+
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+	
+	return 0;
+} // end of main()
